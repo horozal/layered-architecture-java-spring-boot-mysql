34ad024ab4f37d0a8e21ed65dff8b64cd2d521dd
horozal <36187605+horozal@users.noreply.github.com>
2023-02-18 00:57:14 +0100

    Update and rename LayeredarchitecturetemplateApplication.java to StartApplication.java

--- src/main/java/org/eclipse/opensmartclide/layeredarchitecturetemplate/StartApplication.java
+++ src/main/java/org/eclipse/opensmartclide/layeredarchitecturetemplate/StartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LayeredarchitecturetemplateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LayeredarchitecturetemplateApplication.class, args);
+		SpringApplication.run(StartApplication.class, args);
 	}
 
 }
